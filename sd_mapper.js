065aa372029c7ab813cddd6f2e23a30ba47b6c4a
Jose Felix <jfelix000@west-mec.org>
2019-02-12 17:42:32 -0700

    Final code ready for review
    I added all of my comments and made the time current by using the preset. In this code I have finished the specifications.

--- sd_mapper.js
+++ sd_mapper.js
@@ -9,13 +9,17 @@
    Date:   2.12.19
 
 */
-var thisTime = new Date("February 3, 2018 3:15:28");
+// You need to take out the new date so that it changes it to the regular date.
+var thisTime = new Date();
 var timeStr = thisTime.toLocaleString();
 // change the variable for this id to make it timeStr
 document.getElementById("timeStamp").innerHTML = timeStr;
+// THis is how we get the date and time from the other variable "thisTime" variable.
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
 
-var mapNum = ((2*thisMonth + thisHour)%24);
+var mapNum = ((2 * thisMonth + thisHour) % 24 );
+// mapNum and the addition signs change the number that happens based on what the calculations above it were.
 var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
-document.getElementById('planisphere').insertAdjacentElement() = imgStr;
+// this needs to have afterbegin in there as a part of the syntax
+document.getElementById('planisphere').insertAdjacentHTML('afterbegin', imgStr);
